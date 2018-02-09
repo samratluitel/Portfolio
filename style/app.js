694905022f83cb959f1926a4c228f6333583da23
DESKTOP-DR5DFHT\User <ngc2058@gmail.com>
2018-02-09 17:36:03 +0300

    Loading problem fixed

--- style/app.js
+++ style/app.js
@@ -1,6 +1,8 @@
+
 (function ($) {
 $(document).ready(function() {
     // hire smooth scroll
+
     $( window ).resize(function() {
       var ratio = calculateAspectRatioFit()
     });
@@ -82,4 +84,5 @@ $(document).ready(function() {
   new WOW().init();
   $(".owl-next").html('<i class="fa fa-long-arrow-right" aria-hidden="true"></i>');
   $(".owl-prev").html('<i class="fa fa-long-arrow-left" aria-hidden="true"></i>');
+  document.getElementById("hideAll").style.display = "none";
 }(jQuery));
